58814ed7b93bc7e23762fb294013d49dad093cd0
Alexandr.Burmistroff <albook366314@gmail.com>
2023-12-27 16:41:50 +0100

    exception

--- src/main/java/com/burmistrov/task/management/system/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.burmistrov.task.management.system.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
